5fcce15d5647d1ea28f6eb54444ffb0568dcc77a
Andrea <vrlji.andrea@gmail.com>
2022-02-03 09:54:37 -0800

    Create a library project

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from './components/header';
+
+const App = () => {
+   return (<div>
+            <Header/>
+   </div>)
+}
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
